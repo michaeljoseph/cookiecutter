bc61dbd19b8162bed9599949d9e0a8819b3a1a98
Michael Joseph <michaeljoseph+github@gmail.com>
2014-11-20 12:58:51 +0200

    Update cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,24 +2,24 @@ import os
 
 from click.testing import CliRunner
 
-from cookiecutter.cli import main
+from cookiecutter.cli import generate
 
 runner = CliRunner()
 
 
 def test_cli_version():
-    result = runner.invoke(main, ['-V'])
+    result = runner.invoke(generate, ['-V'])
     assert result.exit_code == 0
     assert result.output.startswith('Cookiecutter')
 
 
 def test_cli():
-    result = runner.invoke(main, ['tests/fake-repo-pre/', '--no-input'])
+    result = runner.invoke(generate, ['tests/fake-repo-pre/', '--no-input'])
     assert result.exit_code == 0
     assert os.path.isdir('fake-project')
 
 
 def test_cli_verbose():
-    result = runner.invoke(main, ['tests/fake-repo-pre/', '--no-input', '-v'])
+    result = runner.invoke(generate, ['tests/fake-repo-pre/', '--no-input', '-v'])
     assert result.exit_code == 0
     assert os.path.isdir('fake-project')
