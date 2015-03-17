12ed539512c21de85d17530408b261cd1bce9948
Michael Joseph <michaeljoseph+github@gmail.com>
2015-03-17 10:24:48 +0200

    Adjust import

--- tests/test_abbreviation_expansion.py
+++ tests/test_abbreviation_expansion.py
@@ -18,46 +18,46 @@ TestAbbreviationExpansion.test_abbreviation_expansion_prefix_not_0_in_braces
 from __future__ import unicode_literals
 import pytest
 
-from cookiecutter import main
+from cookiecutter import template
 
 
 def test_abbreviation_expansion():
-    input_dir = main.expand_abbreviations(
+    input_dir = template.expand_abbreviations(
         'foo', {'abbreviations': {'foo': 'bar'}}
     )
     assert input_dir == 'bar'
 
 
 def test_abbreviation_expansion_not_an_abbreviation():
-    input_dir = main.expand_abbreviations(
+    input_dir = template.expand_abbreviations(
         'baz', {'abbreviations': {'foo': 'bar'}}
     )
     assert input_dir == 'baz'
 
 
 def test_abbreviation_expansion_prefix():
-    input_dir = main.expand_abbreviations(
+    input_dir = template.expand_abbreviations(
         'xx:a', {'abbreviations': {'xx': '<{0}>'}}
     )
     assert input_dir == '<a>'
 
 
 def test_abbreviation_expansion_builtin():
-    input_dir = main.expand_abbreviations(
+    input_dir = template.expand_abbreviations(
         'gh:a', {}
     )
     assert input_dir == 'https://github.com/a.git'
 
 
 def test_abbreviation_expansion_override_builtin():
-    input_dir = main.expand_abbreviations(
+    input_dir = template.expand_abbreviations(
         'gh:a', {'abbreviations': {'gh': '<{0}>'}}
     )
     assert input_dir == '<a>'
 
 
 def test_abbreviation_expansion_prefix_ignores_suffix():
-    input_dir = main.expand_abbreviations(
+    input_dir = template.expand_abbreviations(
         'xx:a', {'abbreviations': {'xx': '<>'}}
     )
     assert input_dir == '<>'
@@ -65,4 +65,4 @@ def test_abbreviation_expansion_prefix_ignores_suffix():
 
 def test_abbreviation_expansion_prefix_not_0_in_braces():
     with pytest.raises(IndexError):
-        main.expand_abbreviations('xx:a', {'abbreviations': {'xx': '{1}'}})
+        template.expand_abbreviations('xx:a', {'abbreviations': {'xx': '{1}'}})
