cc26feecd593573abf228a6ddf2be3ada5f59854
michaeljoseph <michaeljoseph+github@gmail.com>
2017-07-01 17:25:01 +0200

    Pull up repo_dir_with_hooks fixture to hooks test module

--- tests/hooks/test_find_hook.py
+++ tests/hooks/test_find_hook.py
@@ -1,60 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import os
-import pytest
 import sys
 import textwrap
 
+import pytest
+
 from cookiecutter import hooks
-
-
-@pytest.yield_fixture
-def repo_dir_with_hooks(tmpdir):
-    """Yield a cookiecutter directory with hook files."""
-
-    repo_dir = tmpdir
-    hooks_dir = repo_dir.mkdir('hooks')
-
-    pre_hook_content = textwrap.dedent(
-        u"""\
-        #!/usr/bin/env python
-        # -*- coding: utf-8 -*-
-        from __future__ import print_function
-
-        print('pre generation hook')
-        f = open('python_pre.txt', 'w')
-        f.close()
-        """
-    )
-    pre_gen_hook_file = hooks_dir / 'pre_gen_project.py'
-    pre_gen_hook_file.write_text(pre_hook_content, encoding='utf8')
-
-    if sys.platform.startswith('win'):
-        post_gen_hook_file = hooks_dir / 'post_gen_project.bat'
-        post_hook_content = textwrap.dedent(
-            u"""\
-            @echo off
-
-            echo post generation hook
-            echo. >shell_post.txt
-            """
-        )
-        post_gen_hook_file.write_text(post_hook_content, encoding='utf8')
-    else:
-        post_gen_hook_file = hooks_dir / 'post_gen_project.sh'
-        post_hook_content = textwrap.dedent(
-            u"""\
-            #!/bin/bash
-
-            echo 'post generation hook';
-            touch 'shell_post.txt'
-            """
-        )
-        post_gen_hook_file.write_text(post_hook_content, encoding='utf8')
-
-    yield str(repo_dir)
-
-    repo_dir.remove()
 
 
 def test_find_hook(monkeypatch, repo_dir_with_hooks):
